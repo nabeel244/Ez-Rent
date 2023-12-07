6eccf986fe47f12c35f6e42b80fdebacdf29f488
Sajjad <56935657+Sajjad750@users.noreply.github.com>
2023-12-08 01:13:54 +0500

    authorization code

--- routes/CategoryRoutes.js
+++ routes/CategoryRoutes.js
@@ -1,13 +1,16 @@
 // routes/CategoryRoutes.js
 const express = require('express');
 const CategoryController = require('../controllers/CategoryController');
+const authenticateToken = require('../middlewares/AuthMiddleware');
+const checkRole = require('../middlewares/RoleMiddleware');
+
 
 const fileHandlingMiddleware = require('../middlewares/FileHandlingMiddleware'); // Adjust the path as necessary
 const router = express.Router();
 
 router.post('/create', fileHandlingMiddleware, CategoryController.createCategory);
 
-router.get('/:id', CategoryController.getCategory);
+router.get('/:id', authenticateToken, checkRole(['admin']), CategoryController.getCategory);
 router.put('/:id', fileHandlingMiddleware, CategoryController.updateCategory);
 router.delete('/:id', CategoryController.deleteCategory);
 
