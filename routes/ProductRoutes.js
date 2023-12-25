fe3be21573c58744a27c5f4bd384174746397d80
nabeetechmate <nabeel@techmatetech.com>
2023-12-26 01:41:43 +0500

    auth changings

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -5,9 +5,11 @@ const productController = require('../controllers/ProductConroller');
 const authenticateToken = require('../middlewares/AuthMiddleware')
 const checkRole = require('../middlewares/RoleMiddleware')
 const router = express.Router();
+const fileHandlingMiddleware = require('../middlewares/FileHandlingMiddleware'); // Adjust the path as necessary
 
 // POST request to create a new product
-router.post('/products', authenticateToken, checkRole(['lessor']), productController.createProduct);
+router.post('/',fileHandlingMiddleware,  productController.createProduct);
+// router.post('/', authenticateToken, checkRole(['lessor']), productController.createProduct);
 
 router.get('/products/:id', authenticateToken, checkRole(['lessor', 'lessee']), productController.getProduct);
 
