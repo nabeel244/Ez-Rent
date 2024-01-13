b1977736c87d6ea53a1f70dafdf2baafd494c2f5
Sajjad Ahmad <m.sajjadahmad763@gmail.com>
2024-01-13 14:34:13 +0500

    Pages code

--- services/PageService.js
+++ services/PageService.js
@@ -1,16 +1,20 @@
 // services/PageService.js
-const { Page } = require('../models/Page'); // Import the Sequelize model
+
 
 const PageService = {
-    async createPage(pageData) {
-        return await Page.create(pageData);
+    async createPage(req) {
+   console.log(req.body);
     },
 
     async getPage(pageName) {
-        return await Page.findOne({ where: { pageName } });
+        try {
+            const page = pages.find(p => p.pageName === pageName);
+            return page;
+        } catch (error) {
+            throw error;
+        }
     },
 
-    // Implement other methods like updatePage, deletePage, allPages, etc.
 };
 
 module.exports = PageService;
